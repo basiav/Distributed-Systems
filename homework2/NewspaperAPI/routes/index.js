72a58b8ca85c43831191fac610355da4bab68bb3
basiav <82549088+basiav@users.noreply.github.com>
2022-03-24 22:10:15 +0100

    Filtering newspapers work

--- homework2/NewspaperAPI/routes/index.js
+++ homework2/NewspaperAPI/routes/index.js
@@ -13,10 +13,11 @@ let title = "Welcome to NewspaperAPI";
 let country;
 const newspapers = config.newspapers;
 let articles = [];
+let chosenNewspaper;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {welcomeTitle: title});
+  res.render('index', {welcomeTitle: title, newspapers: newspapers});
   next()
 });
 
@@ -25,7 +26,7 @@ router.post('/postCountryRequest', (req, res) => {
   country = req.body.country;
   articles = [];
 
-  loadNews(req, res)
+  loadNewsByCountry(req, res)
     .then(res => {
         let msg = "\nArticles successfully fetched. Response: " + res + " \nArticles number: " + articles.length + " \n";
         logMessage("[POST] [/postCountryRequest]", msg, "success");
@@ -36,8 +37,25 @@ router.post('/postCountryRequest', (req, res) => {
     });
 })
 
+/* POST newspapers requests by Newspaper name */
+router.post('/postNewspaperRequest', (req, res) => {
+    chosenNewspaper = req.body.n;
+    country = req.body.country;
+    articles = [];
+
+    loadNewsByNewspaperName(req, res)
+        .then(res => {
+            let msg = "\nArticles successfully fetched for Newspaper name: " + chosenNewspaper +".\nResponse: " + res + " \nArticles number: " + articles.length + " \n";
+            logMessage("[POST] [/postNewspaperRequest]", msg, "success");
+        })
+        .catch(err => {
+            let msg = "\n[POST] ERROR: " + err;
+            logMessage("[POST] [/postNewspaperRequest]", msg, "error");
+        });
+})
+
 /* returns an array of promises, where each promise corresponds to a different newspaper URL */
-function loadNewsPromises(){
+function loadNewsByCountryPromises(){
     const newspaperPromises = [];
     newspapers.forEach(newspaper => {
         let promise = new Promise((resolve, reject) => {
@@ -56,13 +74,12 @@ function loadNewsPromises(){
                             url: newspaper.base + url,
                             source: newspaper.name
                         });
-
                     });
                     resolve();
                 })
                 .catch((err) => {
-                    let msg = "\n[loadNewsPromises] ERROR: " + err;
-                    logMessage("[loadNewsPromises] ", msg, "error");
+                    let msg = "\n[loadNewsByCountryPromises] ERROR: " + err;
+                    logMessage("[loadNewsByCountryPromises] ", msg, "error");
                     reject("Error: ", err);
                 });
         });
@@ -71,17 +88,72 @@ function loadNewsPromises(){
     return newspaperPromises;
 }
 
+/* returns an array of promises, where each promise corresponds to a different newspaper URL */
+function loadNewsByNamePromises(){
+    console.log("CHOSEN NEWSPAPER")
+    const newspaperPromises = [];
+    newspapers
+        .filter(newspaper => newspaper.name === chosenNewspaper)
+        .forEach(newspaper => {
+            let promise = new Promise((resolve, reject) => {
+                axios.get(newspaper.address)
+                    .then(response => {
+                        const html = response.data
+                        const $ = cheerio.load(html)
+
+                        $("a:contains('" + country + "')", html).each(function () {
+                            // console.log($(this).text())
+                            const title = $(this).text()
+                            const url = $(this).attr('href')
+
+                            articles.push({
+                                title,
+                                url: newspaper.base + url,
+                                source: newspaper.name
+                            });
+                        });
+                        resolve();
+                    })
+                    .catch((err) => {
+                        let msg = "\n[loadNewsByNamePromises] ERROR: " + err;
+                        logMessage("[loadNewsByNamePromises] ", msg, "error");
+                        reject("Error: ", err);
+                    });
+            });
+            newspaperPromises.push(promise);
+        });
+    return newspaperPromises;
+}
+
 /* Loads data to the articles array */
-async function loadNews(req, res) {
-    let newspaperPromises = loadNewsPromises(req, res);
+async function loadNewsByCountry(req, res) {
+    let newspaperPromises = loadNewsByCountryPromises(req, res);
     console.log("Loading news for country ... ", country);
 
     for (const promise of newspaperPromises) {
         try {
             await promise;
         } catch (err) {
-            let msg = "\n[loadNews] ERROR: " + err;
-            logMessage("[loadNews] ", msg, "error");
+            let msg = "\n[loadNewsByCountry] ERROR: " + err;
+            logMessage("[loadNewsByCountry] ", msg, "error");
+        }
+    }
+
+    res.render('articles', {country: req.body.country, articles: articles});
+    console.log("ARTICLES", articles);
+}
+
+/* Loads data to the articles array */
+async function loadNewsByNewspaperName(req, res) {
+    let newspaperPromises = loadNewsByNamePromises(req, res);
+    console.log("Loading news for Newspaper ... ", chosenNewspaper);
+
+    for (const promise of newspaperPromises) {
+        try {
+            await promise;
+        } catch (err) {
+            let msg = "\n[loadNewsByNewspaperName] ERROR: " + err;
+            logMessage("[loadNewsByNewspaperName] ", msg, "error");
         }
     }
 
